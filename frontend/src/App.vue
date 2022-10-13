02c66c7fab96bda188e6492bd290306016494706
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-10-13 15:02:57 +0900

    2022-10-13
    프록시등,, 여러 라이브러리 설치
    까먹고 커밋안하면서 계속 진행함

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-      <!-- <q-badge color="{1:orange}" text-color="{2:black}" label="{3:v1.0.0}" /> -->
-    </div>
   </header>
 
   <RouterView />
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-
+// import dns from 'dns'
+// dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
+  transpileDependencies: true,
   plugins: [
     vue({
       template: { transformAssetUrls }
     }),
     quasar()
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+
