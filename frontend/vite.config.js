67d5a35505384be908f64dac310b396e0723b286
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-10-14 16:36:46 +0900

    2022-10-14
    2일째 프록시 부분 해결 못함..
    네트워크 쪽을 살펴보라는 힌트와 port랑 host를 써야한다는 것만 알았다..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,30 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-// import dns from 'dns'
-// dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
-  transpileDependencies: true,
+  server: {
+    host: '0.0.0.0',
+    port: 4173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        secure:false,
+      },
+    }
+  },
   plugins: [
     vue({
       template: { transformAssetUrls }
     }),
     quasar()
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    },
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
-
